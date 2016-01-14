f302d0ecdfe161a2bf0e50e74b520bbfd8a47a6b
harrydoubleyou <harrywhitfeld@gmail.com>
2016-01-14 00:38:39 +0000

    Created pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+# The pages controller contains the code for any page inside of /pages
+class PagesController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def profile
+  end
+
+  def explore
+  end
+end
